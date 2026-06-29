b624a8a472238398e37d000f15bf02fe6a12c193
mohamedamr122 <mohamedamrss123@gmail.com>
2026-06-29 05:16:38 +0300

    fix import issues in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
-import DashboardPage from "../components/pages/DashboardPage/DashboardPage";
-import ProductsPage from "../components/pages/ProductsPage/ProductsPage";
-import UsersPage from "../components/pages/OrdersPage/OrdersPage";
-import AnalyticsPage from "../components/pages/AnalyticsPage/AnalyticsPage";
-import SettingsPage from "../components/pages/SettingsPage/SettingsPage";
-import LoginPage from "../components/pages/LoginPage/LoginPage";
+import DashboardPage from "../components/pages/DashboardPage/DashboardPage.tsx";
+import ProductsPage from "../components/pages/ProductsPage/ProductsPage.tsx";
+import UsersPage from "../components/pages/OrdersPage/OrdersPage.tsx";
+import AnalyticsPage from "../components/pages/AnalyticsPage/AnalyticsPage.tsx";
+import SettingsPage from "../components/pages/SettingsPage/SettingsPage.tsx";
+import LoginPage from "../components/pages/LoginPage/LoginPage.tsx";
 import AuthLayout from "../layouts/AuthLayout";
 import { ProtectedRoute } from "../services/ProtectLayout";
 
